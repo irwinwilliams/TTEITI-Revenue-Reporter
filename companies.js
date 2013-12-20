2daef97def694f8df75191ae20aeaf01918649d4
irwinwilliams <irwin.williams@gmail.com>
2013-12-19 22:21:01 -0400

    First commit of the TTEITI Open Data workshop app

---
+++ companies.js
@@ -0,0 +1,464 @@
+var companies = {
+  "openTTEITI":{
+    "companies":{
+      "company":[
+        {
+          "id":1,
+          "name":"Amoco Trinidad Gas BV Trinidad Branch",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"BP Group"
+        },
+        {
+          "id":2,
+          "name":"Anadarko",
+          "address":null,
+          "country":"United States",
+          "parent":null,
+          "group":null
+        },
+        {
+          "id":3,
+          "name":"Bayfield Energy (Galeota) Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":null
+        },
+        {
+          "id":4,
+          "name":"Bayfield Energy Trinidad Ltd",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":null
+        },
+        {
+          "id":5,
+          "name":"BG International Ltd (Trinidad Branch)",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"BG Group"
+        },
+        {
+          "id":6,
+          "name":"BG Manatee Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"BG Group"
+        },
+        {
+          "id":7,
+          "name":"BG Trinidad 5A Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"BG Group"
+        },
+        {
+          "id":8,
+          "name":"BG Trinidad and Tobago Ltd (Trinidad Branch)",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"BG Group"
+        },
+        {
+          "id":9,
+          "name":"BG Trinidad Central Block Ltd",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"BG Group"
+        },
+        {
+          "id":10,
+          "name":"BG Trinidad EC Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"BG Group"
+        },
+        {
+          "id":11,
+          "name":"BHP Billiton (Trinidad 3A) Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"BHP Billiton Group"
+        },
+        {
+          "id":12,
+          "name":"BHP Billiton Trinidad 2AB",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"BHP Billiton Group"
+        },
+        {
+          "id":13,
+          "name":"BHP Billiton Trinidad 2C Ltd",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"BHP Billiton Group"
+        },
+        {
+          "id":14,
+          "name":"BP EOC Trinidad Branch",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"BP Group"
+        },
+        {
+          "id":15,
+          "name":"BP Trinidad and Tobago LLC Trinidad Branch",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"BP Group"
+        },
+        {
+          "id":16,
+          "name":"Centrica (Horne & Wren) Ltd",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Centrica Group"
+        },
+        {
+          "id":17,
+          "name":"Centrica North Sea Gas Limited",
+          "address":null,
+          "country":"United Kingdom",
+          "parent":null,
+          "group":"Centrica Group"
+        },
+        {
+          "id":18,
+          "name":"Centrica North Sea Oil Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Centrica Group"
+        },
+        {
+          "id":19,
+          "name":"Centrica Resources (Amada) Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Centrica Group"
+        },
+        {
+          "id":20,
+          "name":"Centrica Resources Ltd",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Centrica Group"
+        },
+        {
+          "id":21,
+          "name":"Centrica Trinidad and Tobago Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Centrica Group"
+        },
+        {
+          "id":22,
+          "name":"Centrica Upstream Limited",
+          "address":null,
+          "country":"United Kingdom",
+          "parent":null,
+          "group":"Centrica Group"
+        },
+        {
+          "id":23,
+          "name":"Chaoyang Petroleum 2C Ltd",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":null
+        },
+        {
+          "id":24,
+          "name":"Chevron Texaco Trinidad & Tobago Resources",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":null
+        },
+        {
+          "id":25,
+          "name":"Chevron Trinidad and Tobago Resources SRL",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":null
+        },
+        {
+          "id":26,
+          "name":"Elf Exploration Trinidad BV",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Total Group"
+        },
+        {
+          "id":27,
+          "name":"ENI Trinidad & Tobago Exploration",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"ENI Group"
+        },
+        {
+          "id":28,
+          "name":"ENI Trinidad Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"ENI Group"
+        },
+        {
+          "id":29,
+          "name":"EOG Resources Trinidad Blk 4A Unlimited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"EOG Group"
+        },
+        {
+          "id":30,
+          "name":"EOG Resources Trinidad U(A) Block Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"EOG Group"
+        },
+        {
+          "id":31,
+          "name":"EOG Resources Trinidad Ltd",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"EOG Group"
+        },
+        {
+          "id":32,
+          "name":"EOG Resources U(B) Block Ltd",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"EOG Group"
+        },
+        {
+          "id":33,
+          "name":"Lease Operators Ltd",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":null
+        },
+        {
+          "id":34,
+          "name":"Mora Oil Ventures",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":null
+        },
+        {
+          "id":35,
+          "name":"National Gas Company",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"N.G.C."
+        },
+        {
+          "id":36,
+          "name":"Niko Resources (Block 4B Caribbean) Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Niko Group"
+        },
+        {
+          "id":37,
+          "name":"Niko Resources (Block 5C Caribbean)Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Niko Group"
+        },
+        {
+          "id":38,
+          "name":"Niko Resources (Caribbean) limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Niko Group"
+        },
+        {
+          "id":39,
+          "name":"Niko Resources (NCMA2 Caribbean) Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Niko Group"
+        },
+        {
+          "id":40,
+          "name":"Niko Resources (NCMA3 Caribbean) Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Niko Group"
+        },
+        {
+          "id":41,
+          "name":"Niko Resources (Trinidad and Tobago Ltd)",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Niko Group"
+        },
+        {
+          "id":42,
+          "name":"NSGP (Ensign) Ltd",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Centrica Group"
+        },
+        {
+          "id":43,
+          "name":"Petroleum Company of Trinidad and Tobago Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Petrotrin Group"
+        },
+        {
+          "id":44,
+          "name":"Primera Block 3(B) limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Primera Group"
+        },
+        {
+          "id":45,
+          "name":"Primera Block 4(A) Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Primera Group"
+        },
+        {
+          "id":46,
+          "name":"Primera East Brighton Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Primera Group"
+        },
+        {
+          "id":47,
+          "name":"Primera Modified U(B) Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Primera Group"
+        },
+        {
+          "id":48,
+          "name":"Primera Oil & Gas Ltd",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Primera Group"
+        },
+        {
+          "id":49,
+          "name":"Primera Rock Dome Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Primera Group"
+        },
+        {
+          "id":50,
+          "name":"Repsol E&P T&T Limited",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Repsol Group"
+        },
+        {
+          "id":51,
+          "name":"Sonde Resources Corp",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":null
+        },
+        {
+          "id":52,
+          "name":"Ten Degrees North Operations Company Ltd",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":null
+        },
+        {
+          "id":53,
+          "name":"Total E&P Trinidad Block 3A",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Total Group"
+        },
+        {
+          "id":54,
+          "name":"Total E&P Trinidad BV",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Total Group"
+        },
+        {
+          "id":55,
+          "name":"Trintomar",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Petrotrin Group"
+        },
+        {
+          "id":56,
+          "name":"Venture North Sea Gas Ltd",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Centrica Group"
+        },
+        {
+          "id":57,
+          "name":"Voyager Energy (Trinidad) Ltd",
+          "address":null,
+          "country":"Trinidad and Tobago",
+          "parent":null,
+          "group":"Niko Group"
+        }
+      ]
+    }
+  }
+}
